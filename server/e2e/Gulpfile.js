b15211549dc142848e1c85409d1614f7e1541083
Ankur Kushwaha <ankur.kus1@gmail.com>
2017-03-25 21:22:10 +0530

    packge json fix

--- server/e2e/Gulpfile.js
+++ server/e2e/Gulpfile.js
@@ -1,33 +1,33 @@
-'use strict'; 
+'use strict';
 
 var $, gulp = require('gulp');
 
-var runConfig = require('jsonfile').readFileSync('./run-config.json'),config=require('./config.js');
+var runConfig = require('jsonfile').readFileSync('./run-config.json');
 
 var protractorReport = require('gulp-protractor-cucumber-html-report');
 //var PropertiesReader = require('properties-reader');
 //var properties = PropertiesReader('test.properties');
 
 $ = require('gulp-load-plugins')({
-	pattern : [ 'gulp-*' ]
+	pattern: ['gulp-*']
 });
 
 // run e2e tests - SERVER MUST BE RUNNING FIRST
-gulp.task('e2eTest', function() {
+gulp.task('e2eTest', function () {
 	return gulp.src(runConfig.specs).pipe($.protractor.protractor({
-		configFile : 'protractor-conf.js'
-	})).on('error', function(e) {
+		configFile: 'protractor-conf.js'
+	})).on('error', function (e) {
 		console.log(e);
 	});
 });
 
-gulp.task('htmlReport',['e2eTest'],function(){
+gulp.task('htmlReport', ['e2eTest'], function () {
 	return gulp.src('./report.json')
-    .pipe(protractorReport({
-        dest: 'reports/'
-    }));
+		.pipe(protractorReport({
+			dest: 'reports/'
+		}));
 })
 
 gulp.task('webdriverUpdate', $.protractor.webdriver_update);
 
-gulp.task('default', [ 'e2eTest','htmlReport' ]);
+gulp.task('default', ['e2eTest', 'htmlReport']);
